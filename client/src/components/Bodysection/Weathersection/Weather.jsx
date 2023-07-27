4a2a964cea4c6eca75c7fc30a133e2b9ab8437ae
christianwondeson <christianwondeson22@gmail.com>
2023-07-27 20:34:53 +0300

    updated

--- client/src/components/Bodysection/Weathersection/Weather.jsx
+++ client/src/components/Bodysection/Weathersection/Weather.jsx
@@ -1,34 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axois from "axios";
+
 import "./weather.css";
-// importting icon
-import CloudIcon from "@mui/icons-material/Cloud";
-import Thunder from "@mui/icons-material/ThunderstormTwoTone";
-import Sun from "@mui/icons-material/WbSunny";
+
+import search from "../../../Assets/images/search.png";
+import cloud from "../../../Assets/images/clouds.png";
+import humidity from "../../../Assets/images/humidity.png";
+import wind from "../../../Assets/images/wind.png";
 
 const Weather = () => {
+  const [data, setdata] = useState({
+    celcius: 10,
+    name: "London",
+    humidity: 10,
+    speed: 2,
+  });
+  useEffect(() => {
+    const apiUri =
+      "https://api.openweathermap.org/data/2.5/weather?q=London&appid=6fb037235c5706d594f784b356c4e73b";
+    axois
+      .get(apiUri)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <div className="weatherContanier">
-      <div className="cloudicon">
-        <Thunder className="cloud"/>
-      </div>
-      <div className="weatherFeed">
-        <span className="weatherTitle">today's weather</span>
-        <span className="date">monday</span>
-        <div className="date_in_number">22 march 2023</div>
-      </div>
-      <div className="temprature">
-        <div className="feed">
-          <div className="sunicon">
-            <Sun   className="sun"/>
-            <span className="degree">10 c</span>
-          </div>
-          <div className="weatherInfo">
-            <span className="degree">40% c</span>
-            <span className="degree">6m/sc</span>
-            <span className="degree">90prapm</span>
+    <div className="weatherContainer">
+      <div className="weather">
+        <div className="search">
+          <input
+            className="inputField"
+            type="text"
+            placeholder="Enter City Name"
+          />
+          <button className="btn">
+            <img src={search} alt="" />
+          </button>
+        </div>
+        <div className="weatherInfo">
+          <img src={cloud} alt="" />
+          <h2>22 ℃</h2>
+          <h2>london</h2>
+          <div className="details">
+            <div className="col">
+              <img src={humidity} alt="" />
+              <div className="humidity">
+                <h2>20%</h2>
+                <h2>humditiy</h2>
+              </div>
+            </div>
+            <div className="col">
+              <img src={wind} alt="" />
+              <div className="wind">
+                <h2>2 km/h</h2>
+                <h2>wind speed</h2>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="btn">24hours weekly</div>
       </div>
     </div>
   );
